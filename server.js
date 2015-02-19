11fb603c81052eafe72c5ed8a445e5c0305e9624
Graham Clapham <info@gcdesign.co.uk>
2015-02-19 20:29:36 +0000

    Initial commit.

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express')
+    , http = require('http')
+    , path = require('path')
+    , cors = require("cors"); // Cross Origin Access Sharing, just in case ;
+
+
+var app = express();
+
+app.set('title','red ant');
+
+app.use(express.static(path.join(__dirname, 'app')));
+
+
+/* serves main page */
+app.use("/", express.static(__dirname + 'app/index.html'));
+
+
+var port = process.env.PORT || 5090;
+
+http.createServer(app).listen(port, function(){
+    console.log('Express server listening on port ' + port);
+});
