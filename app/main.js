b1f384928c9c710f488e30bb0d7fed2fd294dfd4
Graham Clapham <info@gcdesign.co.uk>
2015-02-21 15:26:04 +0000

    The json call is working using a hard coded url. I need to ad a global model or similar to contain global vars.

--- app/main.js
+++ app/main.js
@@ -8,6 +8,7 @@ require.config({
         lodash: '../libs/lodash/lodash', // alternative to underscore
         backbone: '../libs/backbone/backbone-min', // https://github.com/amdjs
         sinon: '../libs/sinon/sinon.js',
+        leaflet: 'bower_components/leaflet/dist/leaflet.js',
         // Require.js plugins
         text: '../libs/require/text',
         
--- app/views/stores.js
+++ app/views/stores.js
@@ -5,18 +5,45 @@ define([
     'text!templates/modules/stores.html'
 ], function($, _, Backbone, template) {
 
-    // again - the dependencies were in the wrong order and the 'l' was missing from the template path.
     var StoresView = Backbone.View.extend({
         el: '.content',
-
+        storeData:null,
+        lat:51.511,
+        long:-0.1198,
+        requestStoreData:function(){
+            var _this = this
+            console.log(this)
+            $.ajax({
+                type: 'GET',
+                cache: false,
+                url: _this.getQueryString(),
+                dataType: 'jsonp',
+                jsonpCallback: 'results',
+                success: function(data){
+                  _this.onSuccess(data)
+                },
+                error: function(XHR, textStatus, errorThrown) {
+                   _this.onError(XHR, textStatus, errorThrown);
+                }
+            });
+        },
+        onSuccess:function(data){
+            console.log("Success ",data.stores.store)
+        },
+        onError:function(XHR, textStatus, errorThrown){
+            console.log(XHR, textStatus, errorThrown);
+        },
+        getQueryString : function(){
+            return "http://cloudservices.arcadiagroup.co.uk/storestock/storestock?brand=12556&jsonp_callback=results&lat="+this.lat+"&long=0.1275&dist=50&res=5"
+            // return "http://cloudservices.arcadiagroup.co.uk/storestock/storestock?brand=12556&jsonp_callback=results&lat='errorme'&long=0.1275&dist=50&res=5"
+        },
         render: function() {
-
+            var _this = this;
             $(this.el).html(template);
 
-
-            $('#sendButton').on('click',function(event)
+            $('#store_search').on('click',function(event)
             {
-                $('#result').html("You sent '"+ $('#messageText').val() + "' to this view");
+               _this.requestStoreData()
 
             });
 
